bcf4b5c6b7afb4decfe230fd0c1adbe0da98ceaa
Liran Martfel <liran.martfel@gmail.com>
2026-02-17 15:48:30 -0500

    Add files via upload

---
+++ Homework_1_grades.py
@@ -0,0 +1,4 @@
+grades = [78,92,65,92,81,74,99,67,88,99]
+grades.sort()
+print(f"the 3 highest grades are: {grades[7:10]}")
+print(f"low to high {sorted (grades, reverse=True)}, the lowest grades are: {grades[0:3]} ")
