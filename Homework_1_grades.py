f6af6debdbbb8048657a2accf0b644d726c19e7d
Liran Martfel <liran.martfel@gmail.com>
2026-03-16 14:04:11 +0200

    Update Homework_1_grades.py

--- Homework_1_grades.py
+++ Homework_1_grades.py
@@ -1,4 +1,6 @@
 grades = [78,92,65,92,81,74,99,67,88,99]
 grades.sort()
-print(f"the 3 highest grades are: {grades[7:10]}")
-print(f"low to high {sorted (grades, reverse=True)}, the lowest grades are: {grades[0:3]} ")
+print(f"Low to High {sorted (grades)}\nthe 3 lowest grades are: {grades[0:3]}")
+print(f"High to Low {sorted (grades, reverse=True)}\nthe 3 highest grades are: {grades[7:10]}")
+
+
